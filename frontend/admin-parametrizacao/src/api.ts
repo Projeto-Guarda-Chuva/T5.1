c899ad5a8a2a9a23db6a1056a7296c7be9d77f71
Arthur Detomi <arthurdetomi@gmail.com>
2026-05-14 18:00:56 -0300

    Aplica redirecionamento para login quando token ta expirado

--- frontend/admin-parametrizacao/src/api.ts
+++ frontend/admin-parametrizacao/src/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import type { AxiosResponse, AxiosError } from "axios";
 
 import authStorage from "./utils/authStorage";
+import { ROUTES } from "./utils/routes";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -17,6 +19,22 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+
+  (error: AxiosError) => {
+    const status = error.response?.status;
+
+    if (status === 401) {
+      authStorage.removeToken();
+
+      window.location.href = ROUTES.LOGIN;
+    }
+
+    return Promise.reject(error);
+  },
 );
 
 export default api;
