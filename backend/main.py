935e8e7c89aba0680883f9388b7087af2218390a
Arthur Detomi <arthurdetomi@gmail.com>
2026-06-02 13:02:02 -0300

    configura cors

--- backend/main.py
+++ backend/main.py
@@ -37,12 +37,7 @@ app = FastAPI(title="T5.1 API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:5174",
-        "http://localhost:5174",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
